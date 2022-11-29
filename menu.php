ef9b1a9f15f2483ceb94f93c5809cfeab5e188b7
carlospaulino00 <103070247+carlospaulino00@users.noreply.github.com>
2022-11-29 20:57:45 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+         body{
+            background-color: gray;
+        }
+        .text{
+            text-align: center;
+            position : relative;
+            top: 200px;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <div class="text">
+        <h1>Quem somos??</h1>
+        <p>Somos uma empresa dedicada a você que deseja um ganho de massa muscular e atigir grandes resultados!!!!</p>
+    </div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <Style>
+        .nav {
+            background-color: black;
+        }
+    </Style>
+    <title>Loja suplemntos</title>
+</head>
+<body>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Início</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Suplementos</a>
+            <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Whey</a>
+            <a class="dropdown-item" href="#">Creatina</a>
+            <a class="dropdown-item" href="#">Hipercalorico</a>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link " href="#">Fale conosco</a>
+        </li>
+    </ul>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
